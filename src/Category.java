1e6fa865b34ab1226116b524810144b74863d92b
khue <khue251103@gmail.com>
2023-01-16 01:17:17 +0700

    Add new class ProductUtils Add viewCategoryID and viewCategoryByName function

--- src/Category.java
+++ src/Category.java
@@ -38,38 +38,5 @@ public class Category {
     public String toString() {
         return "Category ID = " + categoryID + ", Name = " + categoryName;
     }
-
-    static ArrayList<Category> viewCategoryList() {
-        try {
-            Scanner fileScanner = new Scanner((new File("category.txt")));
-            ArrayList<Category> categoryList = new ArrayList<Category>();
-            String line;
-
-            while (fileScanner.hasNext()){
-                line = fileScanner.nextLine();
-                StringTokenizer inReader = new StringTokenizer(line, ",");
-
-                if (inReader.countTokens() != 2) {
-                    throw new IOException("Invalid Input Format");
-                } else {
-                    String fileCategoryID = inReader.nextToken();
-                    String fileCategoryName = inReader.nextToken();
-
-                    categoryList.add(new Category(fileCategoryID, fileCategoryName));
-                }
-            }
-
-            fileScanner.close();
-
-            return categoryList;
-
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return null;
-    }
     
 }
